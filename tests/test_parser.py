fc03c93b5cdf4ab56d1acd83459074f3c4c5dcb4
David Griffin <findgriffin@gmail.com>
2021-09-22 22:11:17 -0700

    Add test: TestParser.test_parser_add.

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -7,10 +7,16 @@ class TestParser(unittest.TestCase):
     def test_parser_no_args(self):
         # Given
         input = []
-
         # When
         result = mind.setup(input)
-
         # Then
         self.assertFalse(result.verbose)
         self.assertFalse(result.stuff)
+
+    def test_parser_add(self):
+        # Given
+        input = ["add", "my", "little", "pony"]
+        # When
+        result = mind.setup(input)
+        # Then
+        self.assertListEqual(result.StuffToAdd, input[1:])

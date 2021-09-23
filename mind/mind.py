179b56ee0b52a56c0f4ce7f0e61cbc894850a460
David Griffin <findgriffin@gmail.com>
2021-09-22 19:52:05 -0700

    Implement basic adding and listing functionality.

--- mind/mind.py
+++ mind/mind.py
@@ -1,12 +1,17 @@
-import logging
+from datetime import datetime
+from pathlib import Path
 from typing import Optional
 import argparse
+import logging
+
+
+DEFAULT_PATH = Path("~/.mind.txt").expanduser()
+ADD = "StuffToAdd"
 
 
 def setup(argv) -> argparse.Namespace:
     parser = argparse.ArgumentParser(
         description="Hello! I'm here to mind your stuff for you.")
-
 
     sub_parsers = parser.add_subparsers(help="sub-command help")
 
@@ -19,10 +24,13 @@ def setup(argv) -> argparse.Namespace:
     parser_forget = sub_parsers.add_parser("forget")
     parser_forget.add_argument("StuffToForget", nargs="+")
 
-    parser_tick = sub_parser.add_parser("tick")
+    parser_tick = sub_parsers.add_parser("tick")
     parser_tick.add_argument("StuffToTick", type=str, nargs="+")
 
-    parser.add_argument("stuff", type=str, nargs="*",
+    parser_add = sub_parsers.add_parser("add")
+    parser_add.add_argument(ADD, type=str, nargs="+")
+
+    parser.add_argument("stuff", type=str, nargs="*", default=None,
                         help="Stuff you want to add...")
     parser.add_argument("-v", "--verbose",  action="store_true",
                         help="Enable verbose logging.")
@@ -30,10 +38,43 @@ def setup(argv) -> argparse.Namespace:
     return parser.parse_args(argv)
 
 
-def run(name: Optional[str] = None) -> str:
-    if name:
-        logging.debug("Name given.")
-        return f"Hello, {name}!"
+def ls(db: list[str], filters: Optional[list[str]] = None):
+    print("Currently minding...")
+    for index, row in enumerate(db[:10], 1):
+        print(f"  {index}. {row.strip()}")
+    if len(db) > 10:
+        print("And more...")
+
+
+def get_db(path: Path = DEFAULT_PATH) -> list[str]:
+    if path.exists():
+        logging.debug(f"Opening DB: {path}")
+        with open(path, "r") as db:
+            lines = db.readlines()
+            lines.sort(reverse=True)
+            return lines
     else:
-        logging.debug("No name given.")
-        return "Hello, world!"
+        logging.debug("Creating new, empty DB.")
+        return []
+
+
+def write_db(db: list[str], path: Path = DEFAULT_PATH):
+    logging.debug(f"Writing DB with {len(db)} entries to {path}")
+    with open(path, "w") as output:
+        output.writelines(db)
+
+
+def create_row(stuff: list[str]) -> str:
+    new = f"{datetime.utcnow().isoformat()}, {' '.join(stuff)}\n"
+    logging.debug(f"Writing: {new}")
+    return new
+
+
+def run(args: argparse.Namespace) -> None:
+    if ADD in args:
+        db = get_db()
+        db.insert(0, create_row(args.StuffToAdd))
+        write_db(db)
+    else:
+        db = get_db()
+        ls(db)

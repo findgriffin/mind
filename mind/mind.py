c715a8ede76cb87c282a9c4490144f870385caad
David Griffin <findgriffin@gmail.com>
2021-09-26 22:13:36 -0700

    Add __main__ to mind.py.

--- mind/mind.py
+++ mind/mind.py
@@ -1,3 +1,4 @@
+#! /usr/bin/env python3
 from datetime import datetime
 from enum import Enum
 from pathlib import Path
@@ -6,6 +7,8 @@ from typing import NamedTuple, Callable, Optional
 import argparse
 import logging
 import sqlite3
+import sys
+
 
 ID = "id"
 BODY = "body"
@@ -296,3 +299,15 @@ def run(args: argparse.Namespace) -> list[str]:
         else:
             return do_list(con)
     con.close()
+
+
+if __name__ == "__main__":
+    args = setup(sys.argv[1:])
+    if args.verbose:
+        logging.basicConfig(level=logging.DEBUG,
+                            format="%(levelname)s: %(message)s")
+        logging.info("Verbose logging enabled.")
+    else:
+        logging.basicConfig(level=logging.INFO, format="%(message)s")
+    for line in run(args):
+        print(line)

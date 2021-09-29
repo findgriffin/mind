2abc4d0ee21b8f765181cb6909a9888639d50b11
David Griffin <findgriffin@gmail.com>
2021-09-29 00:17:11 -0700

    Implement a really minimal ORM-ish thing in ormlite.py.

--- scripts/ormlite.py
+++ scripts/ormlite.py
@@ -1,6 +1,10 @@
 #! /usr/bin/env python3
+import datetime
 import sqlite3
 # From https://docs.python.org/3/library/sqlite3.html
+from sqlite3 import Row, Cursor
+from typing import NamedTuple, Optional
+
 
 def create_and_insert():
     con = sqlite3.connect('example.db')
@@ -39,18 +43,50 @@ def parameterized():
     print(cur.fetchall())
     con.close()
 
+
+
+class DBCon():
+
+    def __init__(self, path):
+        self.con = sqlite3.connect(path)
+        self.con.row_factory = sqlite3.Row
+        print(self.con.isolation_level)
+
+    def tx(self, stmt, params=()) -> list[Row]:
+        cur = None
+        with self.con:
+            print(f"tx: {self.con.in_transaction}")
+            cur = self.con.execute(stmt, params).fetchall()
+            print(f"tx: {self.con.in_transaction}")
+        print(f"tx: {self.con.in_transaction}")
+        return cur
+
+    def query(self, stmt, params=()) -> Cursor:
+        return self.con.execute(stmt, params).fetchall()
+
+
 def commit_log():
-    con = sqlite3.connect("log.db")
-    con.execute("CREATE TABLE IF NOT EXISTS log("
+    con = DBCon("log.db")
+    con.tx("CREATE TABLE IF NOT EXISTS log("
                 "sn INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,"
                 "ts INT NOT NULL DEFAULT (cast(strftime('%s', 'now') as INT)),"
-                "body TEXT)")
-    con.commit()
-    con.execute("INSERT INTO log (body) VALUES (:body)",
+                "body TEXT NOT NULL)")
+    con.tx("INSERT INTO log(body) VALUES (:body)",
                 {"body": "Hello, world!"})
-    con.commit()
-    for row in con.execute("SELECT * FROM log"):
+
+    for row in con.query("SELECT * FROM log ORDER BY sn DESC"):
+        print(row[:])
+    for row in con.query("SELECT * FROM log WHERE sn = :num", {"num": 6}):
+        print("Rows are great!")
+        #print(f"{row[0]} {row[1]} {row['body']}")
+        print(f"{tuple(row)}")
+        print(f"{dict(row)}")
+    for row in con.query("SELECT * FROM log ORDER BY sn DESC LIMIT 1"):
         print(row)
+
+def ideal_commit_log():
+    pass
+    #  = connect("log.db")
 #create_and_insert()
 #select()
 #parameterized()

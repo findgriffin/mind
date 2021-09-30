fd99ca6878ffca35d480f718d3333fd1d5e17050
David Griffin <findgriffin@gmail.com>
2021-09-29 21:37:09 -0700

    ormlite.py: Add sequence_number_enforcement example.

--- scripts/ormlite.py
+++ scripts/ormlite.py
@@ -92,10 +92,53 @@ def commit_log():
     for row in con.query("SELECT * FROM log ORDER BY sn DESC LIMIT 1"):
         print(row)
 
+def sequence_number_enforcement():
+    a_inc = "INTEGER PRIMARY KEY AUTOINCREMENT"
+    check = "CHECK (sn > 0)"
+    con = sqlite3.connect(":memory:")
+    t1 = con.execute(f"CREATE TABLE t1(sn {a_inc}, b TEXT)")
+    t2 = con.execute(f"CREATE TABLE t2(sn {a_inc} {check}, b TEXT)")
+    for i in range(10):
+        with con:
+            con.execute(f"INSERT INTO t1 (b) VALUES ('Entry {i}')")
+            con.execute(f"INSERT INTO t2 (b) VALUES ('Entry {i}')")
+    # Leave a gap
+    with con:
+        con.execute(f"INSERT INTO t1 (sn, b) VALUES (15, 'Entry 15')")
+        con.execute(f"INSERT INTO t2 (sn, b) VALUES (15, 'Entry 15')")
+
+    # Now insert taking advantage of autoincrement
+    with con:
+        cur1 = con.execute(f"INSERT INTO t1 (b) VALUES ('Entry 16')")
+        print(f" t1 sn: {cur1.lastrowid}")
+        cur2 = con.execute(f"INSERT INTO t2 (b) VALUES ('Entry 16')")
+        print(f" t2 sn: {cur2.lastrowid}")
+
+    # Now insert in the gap between lastrowid and 10
+    with con:
+        cur1 = con.execute(f"INSERT INTO t1 (sn, b) VALUES (11, 'Entry 17')")
+        print(f" t1 sn: {cur1.lastrowid}")
+        cur2 = con.execute(f"INSERT INTO t2 (sn, b) VALUES (11, 'Entry 17')")
+        print(f" t2 sn: {cur2.lastrowid}")
+
+    # Now insert again using autoincrement
+    with con:
+        cur1 = con.execute(f"INSERT INTO t1 (b) VALUES ('Entry 18')")
+        print(f" t1 sn: {cur1.lastrowid}")
+        cur2 = con.execute(f"INSERT INTO t2 (b) VALUES ('Entry 18')")
+        print(f" t2 sn: {cur2.lastrowid}")
+
+    # Now insert failing check constraint
+    with con:
+        cur1 = con.execute(f"INSERT INTO t1 (sn, b) VALUES (-1, 'Entry 19')")
+        print(f" t1 sn: {cur1.lastrowid}")
+        cur2 = con.execute(f"INSERT INTO t2 (sn, b) VALUES (-1, 'Entry 19')")
+        print(f" t2 sn: {cur2.lastrowid}")
 def ideal_commit_log():
     pass
     #  = connect("log.db")
 #create_and_insert()
 #select()
 #parameterized()
-commit_log()
+#commit_log()
+sequence_number_enforcement()

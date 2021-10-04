95c4673c38d31f6c25381f98bb43187bafb89043
David Griffin <findgriffin@gmail.com>
2021-10-04 10:15:14 -0700

    Add test_lrm.py that tests generic insert method.

---
+++ tests/test_lrm.py
@@ -0,0 +1,19 @@
+""" Test the 'Light Relational Model' (lighter than an ORM) """
+import unittest
+
+from mind import mind
+
+
+class TestLRM(unittest.TestCase):
+
+    def test_insert(self):
+        rcd = mind.Record(0, "", 0, mind.State.ABSENT)
+        stmt = mind.insert("foo", rcd)
+        self.assertEqual(stmt, "INSERT INTO foo (sn, hash, stuff, before) "
+                         "VALUES (:sn, :hash, :stuff, :before)")
+
+    def test_insert_even_if_none(self):
+        rcd = mind.Record(0, None, None, None)
+        stmt = mind.insert("foo", rcd)
+        self.assertEqual(stmt, "INSERT INTO foo (sn, hash, stuff, before) "
+                               "VALUES (:sn, :hash, :stuff, :before)")

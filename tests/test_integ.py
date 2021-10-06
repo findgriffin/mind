3ec9ebcf1dbc789db66c078e228987f5e7fb3268
David Griffin <findgriffin@gmail.com>
2021-10-05 22:42:46 -0700

    Update test_integ for latest schema.

--- tests/test_integ.py
+++ tests/test_integ.py
@@ -32,33 +32,21 @@ class TestInteg(unittest.TestCase):
         self.assertIn(" -> # This is how you Markdown", output[0])
         self.assertTrue(output[0].endswith("Tags [markdown, nohello]"))
 
-    def verify_old_schema_breaks(self, path):
-        # When
-        with self.assertRaises(RuntimeError) as context:
-            with mind.Mind(path, strict=True) as sesh:
-                mind.QueryStuff().execute(sesh)
-        # Then
-        self.assertIn("Error for table", str(context.exception))
-        self.assertIn("Found: ", str(context.exception))
+    def test_old_schemas(self):
+        for i in range(1, 6):
+            path = Path(f"tests/data/schema-v{i}.db")
+            with self.subTest(f"Testing old schema: {path}"):
+                # When
+                with self.assertRaises(RuntimeError) as context:
+                    with mind.Mind(path, strict=True) as sesh:
+                        mind.QueryStuff().execute(sesh)
+                # Then
+                self.assertIn("Error for table", str(context.exception))
+                self.assertIn("Found: ", str(context.exception))
 
-    def test_schema_v1(self):
-        self.verify_old_schema_breaks(Path("tests/data/schema-v1.db"))
-
-    def test_schema_v2(self):
-        self.verify_old_schema_breaks(Path("tests/data/schema-v2.db"))
-
-    def test_schema_v3(self):
-        self.verify_old_schema_breaks(Path("tests/data/schema-v3.db"))
-
-    def test_schema_v4(self):
-        self.verify_old_schema_breaks(Path("tests/data/schema-v4.db"))
-
-    def test_schema_v5(self):
-        self.verify_old_schema_breaks(Path("tests/data/schema-v5.db"))
-
-    def test_schema_v6(self):
+    def test_latest_schema(self):
         # Given
-        path = Path("tests/data/schema-v6.db")
+        path = Path("tests/data/schema-v7.db")
         # When
         with mind.Mind(path, strict=True) as sesh:
             mind.QueryStuff().execute(sesh)

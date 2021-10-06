8fe7cefe912935b09c5dea5e2966380bc2c3cc78
David Griffin <findgriffin@gmail.com>
2021-10-06 10:53:36 -0700

    Add test_db_size (skipped by default).

--- tests/test_perf.py
+++ tests/test_perf.py
@@ -1,10 +1,12 @@
+import logging
 from argparse import Namespace
 from datetime import datetime
 from pathlib import Path
-from random import choices, randint
+from random import choices, randint, random
 from string import ascii_letters, ascii_lowercase, digits
 import os
 import unittest
+from unittest import skip
 
 from mind import mind
 
@@ -54,4 +56,35 @@ class TestPerf(unittest.TestCase):
 
         # Then
         self.assertLess((finish - start).seconds, 1)
+        self.assertFalse(db_path.exists())
+
+    @skip
+    def test_big_db(self):
+        # Given
+        random_chars = "".join(choices(ascii_lowercase, k=11))
+        db_path = Path(f"/tmp/testrun-{random_chars}.db").expanduser()
+        MAX = 10*365*10
+
+        # When
+        start = datetime.now()
+        with mind.Mind(db_path, strict=True) as sesh:
+            for i in range(MAX):
+                logging.info(f"Iteration {i}")
+                if random() < .7:
+                    letters = choices(ascii_letters, k=11)
+                    tag = choices(digits, k=4)
+                    mind.add_content(sesh, content=[f"{letters} #{tag}"])
+                elif random() < .9 and i > 10:
+                    mind.do_tick(
+                        sesh, Namespace(tick=[str(randint(1, int(i/2)))]))
+                elif i > 10:
+                    mind.do_forget(
+                        sesh, Namespace(forget=[str(randint(1, int(i/2)))]))
+        finish = datetime.now()
+
+        # Then
+
+        self.assertLess(os.stat(db_path).st_size / 1024, 5000)
+        os.remove(db_path)
+        self.assertLess((finish - start).seconds, 70)
         self.assertFalse(db_path.exists())

5605b4dd730e5cfaf5a3f06a30f226644b3585bc
David Griffin <dgriffin@arpio.io>
2021-11-13 03:50:00 -0800

    Cleanup TestSqlite:
      * Set in-mem session and stdout context managers in setUp(..) method.
      * Inspired by: https://nedbatchelder.com/blog/201508/using_context_managers_in_test_setup.html

--- tests/test_sqlite.py
+++ tests/test_sqlite.py
@@ -1,5 +1,9 @@
 from argparse import Namespace
 from datetime import datetime
+
+from contextlib import redirect_stdout
+
+import io
 from time import sleep
 import logging
 import random
@@ -12,19 +16,29 @@ from mind.mind import Mind, QueryStuff, add_content, \
     QueryTags, do_list, do_forget, do_tick, do_add, do_show
 
 
+def setup_context(testcase, cm):
+    """Use a contextmanager to setUp a test case."""
+    val = cm.__enter__()
+    testcase.addCleanup(cm.__exit__, None, None, None)
+    return val
+
+
 class TestSQLite(unittest.TestCase):
     MEM = ":memory:"
 
+    def setUp(self) -> None:
+        self.sesh = setup_context(self, Mind(self.MEM, strict=True))
+        self.stdout = setup_context(self, redirect_stdout(io.StringIO()))
+
     def test_get_db_inmem(self):
         # Given / When
-        with Mind(self.MEM, strict=True) as sesh:
-            # Then
-            self.assertFalse(sesh.con.isolation_level)
-            self.assertFalse(sesh.con.in_transaction)
-            cur1 = sesh.con.execute("SELECT * FROM stuff")
-            self.assertEqual(cur1.lastrowid, 1)
-            cur2 = sesh.con.execute("SELECT * FROM tags")
-            self.assertEqual(cur2.lastrowid, 1)
+        # Then
+        self.assertFalse(self.sesh.con.isolation_level)
+        self.assertFalse(self.sesh.con.in_transaction)
+        cur1 = self.sesh.con.execute("SELECT * FROM stuff")
+        self.assertEqual(cur1.lastrowid, 1)
+        cur2 = self.sesh.con.execute("SELECT * FROM tags")
+        self.assertEqual(cur2.lastrowid, 1)
 
     def test_verify_empty(self):
         logging.basicConfig(level=logging.DEBUG)
@@ -34,181 +48,168 @@ class TestSQLite(unittest.TestCase):
 
     def test_add_and_query(self):
         # Given
-        with Mind(self.MEM, strict=True) as sesh:
-            add_content(sesh, ["one"])
-            fetched = QueryStuff().fetchall(sesh)
-            # Then
-            self.assertEqual(fetched[0][1], "one")
-            now = datetime.utcnow().timestamp()
-            # Note: from https://docs.python.org/3/library/datetime.html
-            #  > This does not support parsing arbitrary ISO 8601 strings
-            #  > - it is only intended as the inverse operation of
-            #  > datetime.isoformat()
-            # This is good for testing, it makes it a strict test!
-            self.assertGreater(now * 1e6, fetched[0].id)
+        add_content(self.sesh, ["one"])
+        fetched = QueryStuff().fetchall(self.sesh)
+        # Then
+        self.assertEqual(fetched[0][1], "one")
+        now = datetime.utcnow().timestamp()
+        # Note: from https://docs.python.org/3/library/datetime.html
+        #  > This does not support parsing arbitrary ISO 8601 strings
+        #  > - it is only intended as the inverse operation of
+        #  > datetime.isoformat()
+        # This is good for testing, it makes it a strict test!
+        self.assertGreater(now * 1e6, fetched[0].id)
 
     def test_add_many_and_query(self):
-        with Mind(self.MEM, strict=True) as sesh:
-            for i in range(20):
-                sleep(0.03)
-                add_content(sesh, [f"entry {i}"])
-            # Then
-            fetched = QueryStuff().fetchall(sesh)
-            self.assertEqual(10, len(fetched))
-            self.assertGreater(fetched[0][0], fetched[-1][0])
+        for i in range(20):
+            sleep(0.03)
+            add_content(self.sesh, [f"entry {i}"])
+        # Then
+        fetched = QueryStuff().fetchall(self.sesh)
+        self.assertEqual(10, len(fetched))
+        self.assertGreater(fetched[0][0], fetched[-1][0])
 
-    def test_update_correct_entry(self):
+    @patch("builtins.input", return_value="y")
+    def test_update_correct_entry(self, patched):
         # Given
         to_tick = "some more stuff!!"
         # When
-        with Mind(self.MEM, strict=True) as sesh:
-            add_content(sesh, ["some stuff!!"])
-            add_content(sesh, [to_tick])
-            active_before = QueryStuff().fetchall(sesh)
-            self.assertEqual(2, len(active_before))
-            with patch("builtins.input", return_value="y"):
-                ticked = do_tick(sesh, Namespace(tick="1"))
-            active_after = QueryStuff().fetchall(sesh)
-            self.assertIn(to_tick, ticked[0])
-            self.assertTrue(ticked[0].startswith("Done: "))
-            self.assertEqual(len(ticked), 1)
-            self.assertEqual(1, len(active_after))
-            self.assertNotIn("more", active_after[0][1])
+        add_content(self.sesh, ["some stuff!!"])
+        add_content(self.sesh, [to_tick])
+        active_before = QueryStuff().fetchall(self.sesh)
+        self.assertEqual(2, len(active_before))
+        ticked = do_tick(self.sesh, Namespace(tick="1"))
+        active_after = QueryStuff().fetchall(self.sesh)
+        self.assertIn(to_tick, ticked[0])
+        self.assertTrue(ticked[0].startswith("Done: "))
+        self.assertEqual(len(ticked), 1)
+        self.assertEqual(1, len(active_after))
+        self.assertNotIn("more", active_after[0][1])
 
     def test_add_with_tags(self, strict=True):
-        with Mind(self.MEM) as sesh:
-            add_content(sesh, ["some stuff!!! #stuff"])
-            sleep(.02)
-            add_content(sesh, ["more stuff!!! #thing"])
-            sleep(.02)
-            add_content(sesh, ["less stuff??? #hello"])
-            sleep(.02)
-            add_content(sesh, ["less stuff??? #thing"])
-            latest = QueryTags(id=None).execute(sesh)
-            self.assertListEqual([tag.tag for tag in latest],
-                                 ["thing", "hello", "stuff"])
+        add_content(self.sesh, ["some stuff!!! #stuff"])
+        sleep(.02)
+        add_content(self.sesh, ["more stuff!!! #thing"])
+        sleep(.02)
+        add_content(self.sesh, ["less stuff??? #hello"])
+        sleep(.02)
+        add_content(self.sesh, ["less stuff??? #thing"])
+        latest = QueryTags(id=None).execute(self.sesh)
+        self.assertListEqual([tag.tag for tag in latest],
+                             ["thing", "hello", "stuff"])
 
     def test_get_lots_of_tags(self):
         # Given
         inserted_tags = 20
         expected_tags = 15
         inserted_rows = 40
-        with Mind(self.MEM, strict=True) as sesh:
-            for i in range(inserted_rows):
-                letters = random.choices(string.ascii_letters, k=11)
-                add_content(sesh, [f"{letters} #{i % inserted_tags}"])
-                sleep(.005)
-            # When
-            output = QueryTags(id=None).execute(sesh)
-            # Then
-            self.assertEqual(len(output), expected_tags)
-            for i, tag in enumerate(reversed(output), start=5):
-                self.assertEqual(tag.tag, str(i % inserted_tags))
+        for i in range(inserted_rows):
+            letters = random.choices(string.ascii_letters, k=11)
+            add_content(self.sesh, [f"{letters} #{i % inserted_tags}"])
+            sleep(.005)
+        # When
+        output = QueryTags(id=None).execute(self.sesh)
+        # Then
+        self.assertEqual(len(output), expected_tags)
+        for i, tag in enumerate(reversed(output), start=5):
+            self.assertEqual(tag.tag, str(i % inserted_tags))
 
     def test_do_list_empty(self):
         # Given
-        with Mind(self.MEM, strict=True) as sesh:
-            # When
-            output = do_list(sesh, Namespace(cmd=None, num=1000, page=1))
-            # Then
-            self.assertEqual("  Hmm, couldn't find anything here.", output[2])
+        # When
+        output = do_list(self.sesh, Namespace(cmd=None, num=1000, page=1))
+        # Then
+        self.assertEqual("  Hmm, couldn't find anything here.", output[2])
 
-    def test_forget_success(self):
+    @patch("builtins.input", return_value="y")
+    def test_forget_success(self, patched):
         # Given
         args = Namespace(forget="1")
-        with Mind(self.MEM, strict=True) as sesh:
-            add_content(sesh, ["some content"])
-            # When
-            with patch("builtins.input", return_value="y"):
-                output = do_forget(sesh, args)
-            # Then
-            self.assertEqual(1, len(output))
-            self.assertTrue(output[0].startswith("Hidden: "))
-            self.assertTrue(output[0].endswith(" -> some content"))
+        add_content(self.sesh, ["some content"])
+        # When
+        output = do_forget(self.sesh, args)
+        # Then
+        self.assertEqual(1, len(output))
+        self.assertTrue(output[0].startswith("Hidden: "))
+        self.assertTrue(output[0].endswith(" -> some content"))
 
-    def test_forget_when_empty(self):
+    @patch("builtins.input", return_value="y")
+    def test_forget_when_empty(self, patched):
         # Given
         args = Namespace(forget="1")
-        with Mind(self.MEM, strict=True) as sesh:
-            # When
-            with patch("builtins.input", return_value="y"):
-                output = do_forget(sesh, args)
-            # Then
-            self.assertListEqual(["Stuff with ID 1 not found."], output)
+        # When
+        output = do_forget(self.sesh, args)
+        # Then
+        self.assertListEqual(["Stuff with ID 1 not found."], output)
 
-    def test_forget_tag_indexed(self):
+    @patch("builtins.input", return_value="y")
+    def test_forget_tag_indexed(self, patched):
         # Given
         args = Namespace(forget="#tag.1")
-        with Mind(self.MEM, strict=True) as sesh:
-            # When
-            with self.assertRaises(ValueError):
-                with patch("builtins.input", return_value="y"):
-                    do_forget(sesh, args)
+        # When
+        with self.assertRaises(ValueError):
+            do_forget(self.sesh, args)
 
-    def test_tick_multiple_args(self):
+    @patch("builtins.input", return_value="y")
+    def test_tick_multiple_args(self, patched):
         # Given
         args = Namespace(tick="1,2,3")
-        with Mind(self.MEM, strict=True) as sesh:
-            # When
-            do_add(sesh, Namespace(text="one"))
-            do_add(sesh, Namespace(text="two"))
-            do_add(sesh, Namespace(text="three"))
-            with patch("builtins.input", return_value="y"):
-                ticked = do_tick(sesh, args)
+        # When
+        do_add(self.sesh, Namespace(text="one"))
+        do_add(self.sesh, Namespace(text="two"))
+        do_add(self.sesh, Namespace(text="three"))
+        ticked = do_tick(self.sesh, args)
 
-            # Then
-            listed = do_list(sesh, Namespace(num=10, page=1))
-            self.assertEqual(len(ticked), 3)
-            self.assertIn("  Hmm, couldn't find anything here.", listed)
+        # Then
+        listed = do_list(self.sesh, Namespace(num=10, page=1))
+        self.assertEqual(len(ticked), 3)
+        self.assertIn("  Hmm, couldn't find anything here.", listed)
 
     def test_tick_empty_db(self):
         # Given
         args = Namespace(tick="1")
-        with Mind(self.MEM, strict=True) as sesh:
-            # When
-            output = do_tick(sesh, args)
-            # Then
-            self.assertListEqual(["Stuff with ID 1 not found."], output)
+        # When
+        output = do_tick(self.sesh, args)
+        # Then
+        self.assertListEqual(["Stuff with ID 1 not found."], output)
 
     def test_show_success(self):
         # Given
         args = Namespace(show="1")
-        with Mind(self.MEM, strict=True) as sesh:
-            do_add(sesh, Namespace(text="hello #something"))
-            # When
-            output = do_show(sesh, args)
-            # Then
-            self.assertEqual(len(output), 5)
-            self.assertTrue("Stuff" in output[0])
-            self.assertEqual(output[2], "Tags [something]")
-            self.assertEqual(output[4], "hello")
+        do_add(self.sesh, Namespace(text="hello #something"))
+        # When
+        output = do_show(self.sesh, args)
+        # Then
+        self.assertEqual(len(output), 5)
+        self.assertTrue("Stuff" in output[0])
+        self.assertEqual(output[2], "Tags [something]")
+        self.assertEqual(output[4], "hello")
 
-    def test_filtered_list(self):
+    @patch("builtins.input", return_value="y")
+    def test_filtered_list(self, patched):
         # Given
         sep = ":::"
         original_entries = 30
-        with Mind(self.MEM, strict=True) as sesh:
-            for i in range(original_entries):
-                do_add(sesh, Namespace(text=f"Entry{sep}{i+1} #{i % 2}"))
-            excluded = []
-            for i in range(4):
-                with patch("builtins.input", return_value="y"):
-                    excluded.append(do_tick(sesh, Namespace(tick=f"{2}"))[0])
-                    excluded.append(do_forget(sesh,
-                                              Namespace(forget=f"{2}"))[0])
-            # When
-            tag_0 = do_list(sesh, Namespace(list="0", num=10, page=1))
-            tag_1 = do_list(sesh, Namespace(list="1", num=10, page=1))
+        for i in range(original_entries):
+            do_add(self.sesh, Namespace(text=f"Entry{sep}{i+1} #{i % 2}"))
+        excluded = []
+        for i in range(4):
+            excluded.append(do_tick(self.sesh, Namespace(tick=f"{2}"))[0])
+            excluded.append(do_forget(self.sesh,
+                                      Namespace(forget=f"{2}"))[0])
+        # When
+        tag_0 = do_list(self.sesh, Namespace(list="0", num=10, page=1))
+        tag_1 = do_list(self.sesh, Namespace(list="1", num=10, page=1))
 
-            excluded_set = set([text.split(sep)[-1] for text in excluded])
-            tag_0_set = set([text.split(sep)[-1] for text in tag_0[2:-4]])
-            tag_1_set = set([text.split(sep)[-1] for text in tag_1[2:-4]])
+        excluded_set = set([text.split(sep)[-1] for text in excluded])
+        tag_0_set = set([text.split(sep)[-1] for text in tag_0[2:-4]])
+        tag_1_set = set([text.split(sep)[-1] for text in tag_1[2:-4]])
 
-            self.assertSetEqual(set(excluded_set) & set(tag_0_set), set())
-            self.assertSetEqual(set(excluded_set) & set(tag_1_set), set())
-            self.assertSetEqual(set(tag_0_set) & set(tag_1_set), set())
+        self.assertSetEqual(set(excluded_set) & set(tag_0_set), set())
+        self.assertSetEqual(set(excluded_set) & set(tag_1_set), set())
+        self.assertSetEqual(set(tag_0_set) & set(tag_1_set), set())
 
-            union = excluded_set.union(tag_0_set).union(tag_1_set)
-            self.assertEqual(len(tag_0_set), 10)
-            self.assertEqual(len(tag_1_set), 10)
-            self.assertEqual(len(union), 28)
+        union = excluded_set.union(tag_0_set).union(tag_1_set)
+        self.assertEqual(len(tag_0_set), 10)
+        self.assertEqual(len(tag_1_set), 10)
+        self.assertEqual(len(union), 28)

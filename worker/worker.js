c19321b1cf9e0f3ccce98b029e036c632a8f2fe2
David Griffin <findgriffin@gmail.com>
2022-11-10 19:08:27 -0800

    Mostly implement worker?

--- worker/worker.js
+++ worker/worker.js
@@ -1,10 +1,5 @@
-/**
- * @param {string} USERNAME User name to access the page
- * @param {string} PASSWORD Password to access the page
- * @param {string} REALM A name of an area (a page or a group of pages) to protect.
- * Some browsers may show "Enter user name and password to access REALM"
- */
-
+AUTH = 'Authorization'
+COOKIE_ALGO = {name: "HMAC"}
 // TODO: implement hex conversion https://stackoverflow.com/a/50868276/12376646
 
 // From https://bitcoin.stackexchange.com/questions/52727/
@@ -39,12 +34,32 @@ async function hashPassword(salt, password) {
   return new Uint8Array(myDigest);
 }
 
-async function hasAuthCookie(request) {
-  const cookies = parseCookies(request.headers.get('Cookie') || '');
-  return cookies[AUTH] != null && cookies[AUTH] == EXPECTED;
+async function validateToken(token) {
+  const parts = token.split('.');
+  if (parts.length == 2) {
+    const data = new Uint8Array(toByteArray(parts[0]));
+    const signature = new Uint8Array(toByteArray(parts[1]));
+    return await crypto.subtle.verify(COOKIE_ALGO, KEY, signature, data);
+  } else {
+    return false;
+  }
 }
 
-COOKIE_ALGO = {name: "HMAC"}
+async function isAuthorized(request) {
+  const hdrs = request.headers;
+  const cookies = parseCookies(hdrs.get('Cookie') || '');
+  if (AUTH in cookies) {
+    return validateToken(cookies[AUTH])
+  } else {
+    const auth = (hdrs.get(AUTH) || '').split('=');
+    if (auth.length == 2) {
+      return validateToken(auth[1])
+    } else {
+      console.warn(`bad auth header: ${auth}`)
+    }
+  }
+}
+
 
 async function generateCookie(key) {
   const data = new Uint8Array(32);
@@ -57,10 +72,9 @@ async function generateCookie(key) {
 async function loginUser(body_json) {
   let {user, password } = body_json;
   if (user && password) {
-    console.log(`logging in ${user} ${password}`);
     if (user in LOGIN) {
       if (LOGIN[user] == toHexString(await hashPassword(SALT, password))) {
-        return generateCookie(KEY)
+        return await generateCookie(KEY)
       } else {
         console.log('bad password');
       }
@@ -73,59 +87,50 @@ async function loginUser(body_json) {
   return false;
 }
 
+function getNotFoundResponse() {
+  return new Response("Not found!", {status: 404});
+}
+
+function getUnauthorizedResponse() {
+  return new Response("Unauthorized", {status: 401});
+}
+
 async function handleLogin(request) {
   const body_json = await request.json();
-
   if (request.headers.get('Content-Type') == 'application/json' && body_json) {
     const cookie = await loginUser(body_json);
-    console.log(cookie)
+    // TODO path=/; domain=[DOMAIN]; secure;
+    // https://www.mikestreety.co.uk/blog/using-cloudflare-workers-to-set-a-cookie-based-on-a-get-parameter-or-path/
+    if (cookie) {
+      let resp = new Response('Logged in!');
+      const auth_cookie = `auth=${cookie}`
+      resp.headers.set('Set-Cookie', `${auth_cookie}; Secure; HttpOnly`);
+      resp.headers.append('AuthCookie', auth_cookie);
+      return resp;
+    } else {
+      return getUnauthorizedResponse();
+    }
   } else {
-    console.log('bad content');
-    return false;
+    console.warn('bad content');
+    return getUnauthorizedResponse();
   }
 }
+
 
 async function handleRequest(request) {
-  if (request.headers.has('authorization')) {
-    const authorization = request.headers.get('authorization')
-    console.log(`checking ${authorization}`)
-    const creds = parseCredentials(authorization)
-     if (creds[0] in USERS && users[creds[0]] == creds[1]) {
-       let resp = new Response('Logged in!');
-       resp.headers.set('Set-Cookie', `${AUTH}=${EXPECTED}; Secure; HttpOnly`)
-       return await fetch(BASE)
-     } else {
-       return await fetch(BASE + 'login?result=False')
-     }
-
-  } else {
-    return await fetch(BASE + 'login')
+  if (request.method == 'POST') {
+    if (request.url == `${BASE}/login`) {
+      return await handleLogin(request)
+    } else {
+      return getNotFoundResponse();
+    }
+  } else if (request.method == 'GET') {
+    if (await isAuthorized(request)) {
+      return await fetch(request);
+    } else {
+      return getUnauthorizedResponse();
+    }
   }
-}
-
-/**
- * Break down base64 encoded authorization string into plain-text username and password
- * @param {string} authorization
- * @returns {string[]}
- */
-function parseCredentials(authorization) {
-  const parts = authorization.split(' ')
-  const plainAuth = atob(parts[1])
-  const credentials = plainAuth.split(':')
-  return credentials
-}
-
-/**
- * Helper funtion to generate Response object
- * @param {string} message
- * @returns {Response}
- */
-function getUnauthorizedResponse(message) {
-  let response = new Response(message, {
-    status: 401,
-  })
-  response.headers.set('WWW-Authenticate', `Basic realm="${REALM}"`)
-  return response
 }
 
 addEventListener('fetch', (event) => {

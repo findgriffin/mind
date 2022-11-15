22a133cd48f7a9314b2aedc3e5899d8406785e8c
David Griffin <findgriffin@gmail.com>
2022-11-15 13:10:53 -0800

    app.py: Use QueryStuff directly.

--- mind/app.py
+++ mind/app.py
@@ -3,21 +3,32 @@ from argparse import Namespace
 
 from flask import Flask, jsonify, request
 
+from mind import QueryStuff, Mind, Order, PAGE_SIZE, Phase
 import mind
 
 app = Flask(__name__)
 
+ADD = ''
+NUM = 'num'
+ORDER = 'order'
+PAGE = 'page'
+PHASE = 'phase'
 QUERY = 'query'
+TAG = 'tag'
 
 
-@app.route('/', methods=['POST'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    mnd = Mind(mind.DEFAULT_DB)
     if QUERY in request.json:
-        return jsonify(mind.QueryStuff(**request.json[QUERY]))
-    if 'list' in request.json:
-        result = mind.list_inner(mind.Mind(mind.DEFAULT_DB),
-                                 Namespace(**request.json['list']))
-        return jsonify([mind.Stuff(*r).preview() for r in result])
+        query = request.json[QUERY]
+        order = Order[query[ORDER]] if ORDER in query else Order.LATEST
+        page = int(query[PAGE]) if PAGE in query else 1
+        num = int(query[NUM]) if NUM in query else PAGE_SIZE + 1
+        phase = Phase[query[PHASE]] if PHASE in query else Phase.ACTIVE
+        tag = query[TAG] if TAG in query and query[TAG].isalnum() else None
+        return jsonify(QueryStuff(order=order, offset=(page - 1) * num,
+                                  state=phase, tag=tag).fetchall(mnd))
 
 
 app.run()

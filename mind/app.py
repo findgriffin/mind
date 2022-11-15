8d505c64f5372c51ed51a212023eb2c3799b280e
David Griffin <findgriffin@gmail.com>
2022-11-14 21:44:56 -0800

    app.py: Add query functionality!.

--- mind/app.py
+++ mind/app.py
@@ -4,15 +4,20 @@ from argparse import Namespace
 from flask import Flask, jsonify, request
 
 import mind
-from cli import COMMANDS
 
 app = Flask(__name__)
 
+QUERY = 'query'
+
+
 @app.route('/', methods=['POST'])
 def index():
+    if QUERY in request.json:
+        return jsonify(mind.QueryStuff(**request.json[QUERY]))
     if 'list' in request.json:
         result = mind.list_inner(mind.Mind(mind.DEFAULT_DB),
                                  Namespace(**request.json['list']))
         return jsonify([mind.Stuff(*r).preview() for r in result])
+
 
 app.run()

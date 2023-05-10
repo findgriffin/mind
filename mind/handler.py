788053d0dc89eb8fe78407a0bdac1874c54e5a01
David Griffin <findgriffin@gmail.com>
2023-05-10 16:46:39 -0700

    Make hello world handler work.

--- mind/handler.py
+++ mind/handler.py
@@ -3,4 +3,8 @@ import json
 
 def handler(event, context):
     print(json.dumps(event))
-    print(json.dumps(context))
+    print("Lambda function ARN:", context.invoked_function_arn)
+    print("CloudWatch log stream name:", context.log_stream_name)
+    print("CloudWatch log group name:",  context.log_group_name)
+    print("Lambda Request ID:", context.aws_request_id)
+    return "Hello, world!"
